72ce89ea5146886b7e3c6d832db6b9b25c985eaf
armagan <armagan.sahin95@gmail.com>
2024-08-14 21:38:58 +0300

    section 2

---
+++ section2/loans/src/main/java/com/eazybytes/loans/mapper/LoansMapper.java
@@ -0,0 +1,28 @@
+package com.eazybytes.loans.mapper;
+
+import com.eazybytes.loans.dto.LoansDto;
+import com.eazybytes.loans.entity.Loans;
+
+public class LoansMapper {
+
+    public static LoansDto mapToLoansDto(Loans loans, LoansDto loansDto){
+        loansDto.setLoanType(loans.getLoanType());
+        loansDto.setLoanNumber(loans.getLoanNumber());
+        loansDto.setTotalLoan(loans.getTotalLoan());
+        loansDto.setMobileNumber(loans.getMobileNumber());
+        loansDto.setAmountPaid(loans.getAmountPaid());
+        loansDto.setOutstandingAmount(loans.getOutstandingAmount());
+        return loansDto;
+    }
+
+    public static Loans mapToLoans(LoansDto loansDto, Loans loans){
+        loans.setLoanType(loansDto.getLoanType());
+        loans.setLoanNumber(loansDto.getLoanNumber());
+        loans.setTotalLoan(loansDto.getTotalLoan());
+        loans.setMobileNumber(loansDto.getMobileNumber());
+        loans.setAmountPaid(loansDto.getAmountPaid());
+        loans.setOutstandingAmount(loansDto.getOutstandingAmount());
+        return loans;
+    }
+
+}
